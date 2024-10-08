d98bf38940def661037912ff10502d25980c183f
Chris <christian@culling.se>
2024-10-08 18:39:04 +0200

    adds print

--- Person.cpp
+++ Person.cpp
@@ -1,9 +1,28 @@
-#include "Person.h"
+using namespace std;
+
+#include "person.h"
+#include <iostream>
 
 class Person
 {
+    string name;
+    string address;
+
+public:
+
     Person(string name, string address)
     {
+        this->name = name;
+        this->address = address;
+    }
+    
+    Person()
+    {
         // implement constructor
+    }
+
+    void Print()
+    {
+        cout << name << ", " << address << endl;
     }
 };
